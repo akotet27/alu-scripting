8661fbf025ba0970e1d7ef4c99cd78c182f557c9
akotet27 <a.demise@alueducation.com>
2026-05-16 13:43:39 +0200

    Refactor textme.rb to improve input handling for regex scanning

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV.join(' ').scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+input = ARGV.empty? ? STDIN.read : ARGV.join(' ')
+puts input.scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
